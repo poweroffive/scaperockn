7cf4d1896dd370532f8a2518e1b24abf094205e0
christopherrolfe198 <mightyguy198@googlemail.com>
2013-01-07 17:36:57 +0000

    redirect from deleting a post

--- forum/deletepost.php
+++ forum/deletepost.php
@@ -3,14 +3,14 @@
 if($_SESSION['team'] == "SL" || $_SESSION['team'] == "Community"){
 	$redirect = rtrim($http_referer, "true");
 
-	$forumid = $_GET['forumid'];
-	$threadid = $_GET['threadid'];
+	$forumid = $_SESSION['forumid'];
+	$threadid = $_SESSION['threadid'];
 	$post_id = $_GET['deletepost'];
 
-	echo $query = "UPDATE postsection SET status='hidden' where post_id=$post_id AND thread_id=$threadid AND forum_id=$forumid";
+	$query = "UPDATE postsection SET status='hidden' where post_id=$post_id AND thread_id=$threadid AND forum_id=$forumid";
 	if(mysql_query($query)){
 		echo "Post has been archived and deleted from the forums. You will be re-directed in 5 seconds.";
-		echo $deleted = 'true';
+		$deleted = 'true';
 		header("Location:$redirect");
 	} else {
 		echo "There was an issue please contact the site admin";
